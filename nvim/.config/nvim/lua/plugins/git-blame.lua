9cda3159646a4faf916b6258421597fb6e7604a6
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-09-02 21:05:01 -0400

    neovim

--- nvim/.config/nvim/lua/plugins/git-blame.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'f-person/git-blame.nvim',
-    event = {'BufReadPost', 'BufNewFile'},
-    enabled = false, -- using gitsigns built-in functionality for now
-    init = function()
-        vim.g.gitblame_message_template = ' <author> • <date> • <summary>'
-        vim.g.gitblame_enabled = 0
-    end
-}
