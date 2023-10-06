88a79883723b99f032a8d7b6049796f362b73864
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-06 12:50:57 -0400

    R-specific settings

---
+++ nvim/.config/nvim/after/queries/r/textobjects.scm
@@ -0,0 +1,7 @@
+; from echasnovski
+; Queries for R tree-sitter textobjects
+(left_assignment (identifier) (function_definition (formal_parameters) (_) @function.inner )) @function.outer
+(equals_assignment (identifier) (function_definition (formal_parameters) (_) @function.inner )) @function.outer
+(super_assignment (identifier) (function_definition (formal_parameters) (_) @function.inner )) @function.outer
+
+(binary (_) (_) @op (#eq? @op "%>%")) @pipe
