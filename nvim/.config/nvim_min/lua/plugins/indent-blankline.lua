cad4f89e41e9be4b82d3f5b238b0826e20e89784
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-10-18 22:34:39 -0400

    nvim

---
+++ nvim/.config/nvim_min/lua/plugins/indent-blankline.lua
@@ -0,0 +1,26 @@
+return {
+    'lukas-reineke/indent-blankline.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    -- version = '2.20.8',
+    -- pin = true,
+    -- config = function()
+    --     require('indent_blankline').setup({
+    --         -- char = '▏', -- U+258F (1/8 block); the default is '│'; note that it blocks insert mode cursor
+    --         -- context_char = '▎', -- U+258E (1/4 block)
+    --         show_current_context = true, -- highlight current context indent
+    --     })
+    -- end
+    main = 'ibl',
+    config = function()
+        require('ibl').setup({
+            indent = {
+                char = '│',
+            },
+            scope = {
+                char = '│',
+                show_start = false,
+                show_end = false,
+            },
+        })
+    end,
+}
