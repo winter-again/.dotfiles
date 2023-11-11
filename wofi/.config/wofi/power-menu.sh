8e5c2f7c2d7491c50ecc9a4b01af40c66046c72d
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2023-11-11 03:44:17 -0500

    Power menu script for wofi. Still need to test out main swaylock instead of AUR fork since that makes system unresponsive on resume.

---
+++ wofi/.config/wofi/power-menu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# source: https://github.com/luispabon/sway-dotfiles/blob/1a255c86a65e9f3059bac0eea137875b22a5e787/scripts/wofi-power.sh#L4
+entries="󰌾 Lock\n󰍃 Logout\n󰒲 Suspend\n Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries | wofi --width 500 --height 250 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+    lock)
+        swaylock --config ~/.config/swaylock/config;;
+    logout)
+        hyprctl dispatch exit;;
+    suspend)
+        exec systemctl suspend;;
+    reboot)
+        exec systemctl reboot;;
+    shutdown)
+        exec systemctl poweroff;;
+esac
