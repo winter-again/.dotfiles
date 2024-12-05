5c70df0b8bfb5195c2c32cf7e9ef8bc3c60fed99
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-12-04 19:03:25 -0500

    Updates

--- tofi/.config/tofi/power-menu.sh
+++ tofi/.config/tofi/power-menu.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-case $(printf "%s\n" "Power off" "Reboot" "Suspend" "Lock" "Log out" | tofi --width 260 --height 260) in
+case $(printf "%s\n" "Power off" "Reboot" "Suspend" "Hibernate" "Lock" "Log out" | tofi --width 260 --height 260) in
     "Power off")
         exec systemctl poweroff;;
     "Reboot")
         exec systemctl reboot;;
     "Suspend")
         exec systemctl suspend;;
+    "Hibernate")
+        exec systemctl hibernate;;
     "Lock")
         gtklock -t "%I:%m %p";;
-    "Log out")
+    "Exit Sway")
         swaymsg exit;;
 esac
