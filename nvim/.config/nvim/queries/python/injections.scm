c8c924d323f605b47f20d00286a0f9dbe24c68db
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-06-05 17:53:27 -0400

    Updates

--- nvim/.config/nvim/queries/python/injections.scm
+++ nvim/.config/nvim/queries/python/injections.scm
@@ -1,9 +1,7 @@
 ;; extends
-; Inject into python SQL queries in multiline strings, assigned to var
-; specifically when variable name contains "query"
-; this also makes it so that SQL queries inside of python that is
-; inside of a markdown fenced code block are also highlighted
-(assignment
-  left: (identifier) @query_name (#lua-match? @query_name "query")
-  right: (string (string_content) @injection.content (#set! injection.language "sql"))
+; inject SQL highlighting into Python strings
+(string 
+  (string_content) @injection.content
+    (#vim-match? @injection.content "^\s*SELECT|FROM|(INNER |LEFT )?JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER|ORDER BY.*$")
+    (#set! injection.language "sql")
 )
