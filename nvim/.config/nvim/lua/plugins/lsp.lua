ddf9f96e5d504f27b20d0fbae82c278cd10b9e0c
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-08-24 22:10:57 -0400

    Updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -108,12 +108,11 @@ return {
             local orig_util_open_float_prev = vim.lsp.util.open_floating_preview
             vim.lsp.util.open_floating_preview = function(contents, syntax, opts, ...)
                 opts = opts or {}
-                -- opts.border = 'rounded'
+                -- opts.border = "rounded"
                 return orig_util_open_float_prev(contents, syntax, opts, ...)
             end
 
-            -- see docs: https://neovim.io/doc/user/diagnostic.html
-
+            -- NOTE: https://neovim.io/doc/user/diagnostic.html
             -- custom signs for diagnostics
             -- trouble.nvim can use too
             local diagnostic_icons = {
