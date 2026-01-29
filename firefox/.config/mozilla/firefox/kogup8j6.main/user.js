f2a753711aa0935e1dc66ed18262158fe0ec14cb
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2026-01-28 20:36:26 -0500

    Fastfox tweaks

--- firefox/.config/mozilla/firefox/kogup8j6.main/user.js
+++ firefox/.config/mozilla/firefox/kogup8j6.main/user.js
@@ -1451,15 +1451,15 @@ user_pref("browser.tabs.hoverPreview.showThumbnails", false);
 // XDG desktop portal features
 // 0 = never, 1 = always, 2 = auto
 // file picker
-// user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
 // mime handler
-// user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
+user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
 // settings/look-and-feel information
-// user_pref("widget.use-xdg-desktop-portal.settings", 1);
+user_pref("widget.use-xdg-desktop-portal.settings", 1);
 // geolocation
-// user_pref("widget.use-xdg-desktop-portal.location", 0);
+user_pref("widget.use-xdg-desktop-portal.location", 0);
 // opening to a file
-// user_pref("widget.use-xdg-desktop-portal.open-uri", 1);
+user_pref("widget.use-xdg-desktop-portal.open-uri", 1);
 
 // sidebar
 // activate sidebar
@@ -1473,42 +1473,40 @@ user_pref("sidebar.main.tools", "bookmarks");
 user_pref("sidebar.animation.enabled", false);
 
 // NOTE: Fastfox
-// user_pref("gfx.content.skia-font-cache-size", 32);
+user_pref("gfx.content.skia-font-cache-size", 32);
 
 /** GFX ***/
-// user_pref("gfx.webrender.layer-compositor", true);
-// user_pref("gfx.canvas.accelerated.cache-items", 32768);
-// user_pref("gfx.canvas.accelerated.cache-size", 4096);
-// user_pref("webgl.max-size", 16384);
-
-/** DISK CACHE ***/
-// user_pref("browser.cache.disk.enable", false);
+user_pref("gfx.webrender.layer-compositor", true);
+user_pref("gfx.canvas.accelerated.cache-items", 32768);
+user_pref("gfx.canvas.accelerated.cache-size", 4096);
+user_pref("webgl.max-size", 16384);
 
 /** MEMORY CACHE ***/
-// user_pref("browser.cache.memory.capacity", 131072);
-// user_pref("browser.cache.memory.max_entry_size", 20480);
-// user_pref("browser.sessionhistory.max_total_viewers", 4);
-// user_pref("browser.sessionstore.max_tabs_undo", 10);
+user_pref("browser.cache.memory.capacity", 131072);
+user_pref("browser.cache.memory.max_entry_size", 20480);
+user_pref("browser.sessionhistory.max_total_viewers", 4);
+user_pref("browser.sessionstore.max_tabs_undo", 10);
 
 /** MEDIA CACHE ***/
-// user_pref("media.memory_cache_max_size", 262144);
-// user_pref("media.memory_caches_combined_limit_kb", 1048576);
-// user_pref("media.cache_readahead_limit", 600);
-// user_pref("media.cache_resume_threshold", 300);
+// increase over limit set in 1002; seems to help image loading
+user_pref("media.memory_cache_max_size", 262144);
+user_pref("media.memory_caches_combined_limit_kb", 1048576);
+user_pref("media.cache_readahead_limit", 600);
+user_pref("media.cache_resume_threshold", 300);
 
 /** IMAGE CACHE ***/
-// user_pref("image.cache.size", 10485760);
-// user_pref("image.mem.decode_bytes_at_a_time", 65536);
+user_pref("image.cache.size", 10485760);
+user_pref("image.mem.decode_bytes_at_a_time", 65536);
 
 /** NETWORK ***/
-// user_pref("network.http.max-connections", 1800);
-// user_pref("network.http.max-persistent-connections-per-server", 10);
-// user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
-// user_pref("network.http.request.max-start-delay", 5);
-// user_pref("network.http.pacing.requests.enabled", false);
-// user_pref("network.dnsCacheEntries", 10000);
-// user_pref("network.dnsCacheExpiration", 3600);
-// user_pref("network.ssl_tokens_cache_capacity", 10240);
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.request.max-start-delay", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheEntries", 10000);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
 
 // NOTE: Peskyfox
 // AI
