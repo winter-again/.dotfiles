4ac26a101ba0eb736089dda515e28598415ac7e4
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:09:54 -0500

    nvim Comment

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -3,14 +3,23 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     config = function()
         require('Comment').setup({
-            -- note that these are set using "_" but are actually targeting "/"
+            -- LHS of toggle mappings in normal mode
+            -- toggles commenting for current line
             toggler = {
-                line = '<C-_>',
+                -- line = '<C-_>',
+                line = 'gcc',
+                block = 'gcb',
             },
+            -- LHS of operator-pending maps in normal and visual mode
+            -- can use to comment two lines down with 'gb2j' for ex or after selection
             opleader = {
-                line = '<C-_>',
+                -- line = '<C-_>',
+                line = 'gc',
+                block = 'gb',
             },
             extra = {
+                above = 'gcO',
+                below = 'gco',
                 eol = 'gce',
             },
             pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
