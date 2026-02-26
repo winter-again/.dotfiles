5ec195b97d054d3130f1f783e291d7c2ec566efc
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:10:02 -0500

    Updates

--- nvim/.config/nvim/lua/plugins/git/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "sindrets/diffview.nvim",
-    -- event = { 'BufReadPost', 'BufNewFile' },
-    keys = { "<space>dv" },
-    config = function()
-        vim.keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<CR>", { silent = true, desc = "Open Diffview" })
-        vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory<CR>", { silent = true, desc = "Open Diffview" })
-    end,
-}
