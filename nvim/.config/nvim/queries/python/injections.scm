5f02c9fcac14fffeb823371ca266795e8f484ff6
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:09:57 -0500

    Add python treesitter injection query for highlighting SQL queries inside of Python. Also applies to Python code inside of markdown fenced code blocks

---
+++ nvim/.config/nvim/queries/python/injections.scm
@@ -0,0 +1,8 @@
+; Inject into python SQL queries in multiline strings, assigned to var
+; specifically when variable name contains "query"
+; this also makes it so that SQL queries inside of python that is
+; inside of a markdown fenced code block are also highlighted
+(assignment
+  left: (identifier) @query_name (#lua-match? @query_name "query")
+  right: (string (string_content) @injection.content (#set! injection.language "sql"))
+)
