7c925caf4358cb38941e995bbe18834f1e5bf200
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:10:03 -0500

    Updates

--- nvim/.config/nvim/lua/winteragain/snippets/python.lua
+++ nvim/.config/nvim/lua/winteragain/snippets/python.lua
@@ -34,6 +34,19 @@ return {
         )
     ),
     s(
+        { trig = "match", snippetType = "autosnippet", desc = "match statement" },
+        fmta(
+            [[
+        match <cond>:
+            case <c1>:
+                <o1>
+            case _:
+                <default>
+        ]],
+            { cond = i(1), c1 = i(2), o1 = i(3), default = i(4) }
+        )
+    ),
+    s(
         { trig = "cla", desc = "class" },
         fmta(
             [[
@@ -65,6 +78,35 @@ return {
             <field>: <field_type>
         ]],
             { name = i(1), field = i(2), field_type = i(3) }
+        )
+    ),
+    s(
+        { trig = ".fil", desc = "Polars multi-condition filter" },
+        fmta(
+            [[
+        .filter(
+            (pl.col(<col1>) <out1>)
+            & (pl.col(<col2>) <out2>)
+        )
+        ]],
+            { col1 = i(1), out1 = i(2), col2 = i(3), out2 = i(4) }
+        )
+    ),
+    s(
+        { trig = ".when", snippetType = "autosnippet", desc = "Polars when, then, otherwise construct" },
+        fmta(
+            " .when(pl.col(<col1>) <cond>).then(pl.col(<col2>) <out>).otherwise(pl.col(<otherwise>))",
+            { col1 = i(1), cond = i(2), col2 = i(3), out = i(4), otherwise = i(5) }
+        )
+    ),
+    s(
+        { trig = "with pl", snippetType = "autosnippet", desc = "Polars special print" },
+        fmta(
+            [[
+        with pl.Config(tbl_<opt>=-1):
+            print(<out>)
+        ]],
+            { opt = i(1), out = i(2) }
         )
     ),
 }
