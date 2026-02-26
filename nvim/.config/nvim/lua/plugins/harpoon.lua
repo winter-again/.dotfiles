7c47a3f09f6561363653abfd5c249a2a91dc38c8
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:09:55 -0500

    updates, harpoon v2

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -4,25 +4,27 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
     },
-    enabled = false,
+    branch = 'harpoon2',
     config = function()
-        require('harpoon').setup()
-
-        local mark = require('harpoon.mark')
-        local ui = require('harpoon.ui')
-        vim.keymap.set('n', '<leader>a', mark.add_file)
-        vim.keymap.set('n', '<C-m>', ui.toggle_quick_menu)
+        local harpoon = require('harpoon')
+        harpoon:setup()
+        vim.keymap.set('n', '<leader>a', function()
+            harpoon:list():append()
+        end)
+        vim.keymap.set('n', '<leader>e', function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
         vim.keymap.set('n', '<leader>h', function()
-            ui.nav_file(1)
+            harpoon:list():select(1)
         end)
         vim.keymap.set('n', '<leader>j', function()
-            ui.nav_file(2)
+            harpoon:list():select(2)
         end)
         vim.keymap.set('n', '<leader>k', function()
-            ui.nav_file(3)
+            harpoon:list():select(3)
         end)
         vim.keymap.set('n', '<leader>l', function()
-            ui.nav_file(4)
+            harpoon:list():select(4)
         end)
     end,
 }
