abcd86aa075af425b4fbc719bbc6ea167a81f7ce
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:10:03 -0500

    More updates

--- setup.sh
+++ setup.sh
@@ -2,26 +2,55 @@
 
 set -e
 
-# NOTE: post-install; run as new user
+# NOTE: post-install as root
 
 BUILD_REPOS="$HOME/Documents/build-repos"
 PARU_REPO="$BUILD_REPOS/paru"
 
-read -r -p "Enter username: " USER
-read -r -p "Enter hostname: " HOSTNAME
+pacman_pkg() {
+    pacman --noconfirm --needed -S "$1"
+}
+
+paru_pkg() {
+    paru -S --needed --noconfirm "$1"
+}
+
+setup_and_switch_user() {
+    clear
+    read -r -p "Enter username: " USER
+
+    read -r -p "Enter user password: " PASSWD
+    read -r -p "Confirm password: " PASSWD2
+    while ! [ "$PASSWD" = "$PASSWD2" ]; do
+        read -r -p "Enter user password: " PASSWD
+        read -r -p "Confirm password: " PASSWD2
+    done
+
+    echo "Creating user..."
+    useradd -m -G wheel -s /usr/bin/zsh "$USER"
+
+    echo "Setting user password..."
+    echo "$USER:$PASSWD" | chpasswd
+    unset PASSWD PASSWD2
+
+    echo "Making wheel group sudoers..."
+    sed -i "/%wheel ALL=(ALL:ALL) ALL/s/^# //g" /etc/sudoers
+    # equiv?
+    # echo "%wheel ALL=(ALL:ALL) ALL" | EDITOR="tee -a" visudo
+    
+    echo "Switching to user '$USER'"
+    su -l "$USER"
+}
 
 echo "Make sure to verify boot mode and connect to internet"
 read -r -p "Are you ready to begin? (y/n): " confirm && [[ $confirm = [yY] ]]
 
-cd "$HOME"
-
 echo "Checking internet connection..." 
 ping -c 3 archlinux.org || (echo "Not connected to internet" && exit 1)
 
-# TODO: might not need
-# echo "Configuring timesyncd..."
-# sed -i "s/^#NTP=.*/NTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org" /etc/systemd/timesyncd.conf
-# sed -i "s/^#FallbackNTP=.*/FallbackNTP=0.north-america.pool.ntp.org 1.north-america.pool.ntp.org 2.north-america.pool.ntp.org 3.north-america.pool.ntp.org" /etc/systemd/timesyncd.conf
+echo "Configuring timesyncd..."
+sed -i "s/^#\{0,1\}NTP=.*/NTP=0.arch.pool.ntp.org 1.arch.pool.ntp.org 2.arch.pool.ntp.org 3.arch.pool.ntp.org" /etc/systemd/timesyncd.conf
+sed -i "s/^#\{0,1\}FallbackNTP=.*/FallbackNTP=0.north-america.pool.ntp.org 1.north-america.pool.ntp.org 2.north-america.pool.ntp.org 3.north-america.pool.ntp.org" /etc/systemd/timesyncd.conf
 
 echo "Ensuring that time sync is active..."
 timedatectl set-ntp true
@@ -29,27 +58,48 @@ echo -e "\nDate/Time service status:"
 timedatectl status
 sleep 4
 
-# TODO: might not need
-# echo "Enabling NetworkManager.service"
-# systemctl enable --now NetworkManager.service
-#
-# echo "Configuring /etc/hosts..."
-# cat <<EOF > /etc/hosts
-# 127.0.0.1    localhost
-# ::1          localhost
-# 127.0.1.1    $HOSTNAME
-# EOF
+echo "Configuring hostname..."
+read -r -p "Enter hostname: " HOSTNAME
+echo "$HOSTNAME" > /etc/hostname
+cat <<EOF > /etc/hosts
+127.0.0.1    localhost
+::1          localhost
+127.0.1.1    $HOSTNAME
+EOF
+
+echo "Enabling NetworkManager.service"
+systemctl enable --now NetworkManager.service
+
+echo "Disabling suspend on lid close..."
+sed -i "s/^#\{0,1\}HandleLidSwitch=.*/HandleLidSwitch=ignore" /etc/systemd/logind.conf
+sed -i "s/^#\{0,1\}HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=ignore" /etc/systemd/logind.conf
+sed -i "s/^#\{0,1\}HandleLidSwitchDocked=.*/HandleLidSwitchDocked=ignore" /etc/systemd/logind.conf
+
+echo "Disabling wake on lid open..."
+cat <<EOF > /etc/systemd/system/toggle-lid-wakeup.service
+[Unit]
+Description="Disable LID0 wake from suspend trigger"
+
+[Service]
+ExecStart=/bin/sh -c "/bin/echo LID0 > /proc/acpi/wakeup"
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 # pacman
 echo "Configuring pacman..."
 sed -i "/Color/s/^#//g" /etc/pacman.conf
 sed -i "/VerbosePkgLists/s/^#//g" /etc/pacman.conf
 
+echo "Use all cores for makepkg compilation..."
+sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
+
 echo "Updating before proceeding..."
 pacman -Syu
 
-echo "Installing reflector and activating reflector.timer"
-pacman -S --needed --noconfirm reflector
+echo "Installing and configuring reflector. Activating reflector.timer..."
+pacman_pkg reflector
 systemctl enable --now reflector.timer
 cat <<EOF > /etc/xdg/reflector/reflector.conf
 --save /etc/pacman.d/mirrorlist
@@ -59,19 +109,86 @@ cat <<EOF > /etc/xdg/reflector/reflector.conf
 --sort rate
 EOF
 
-echo "Installing pacman-contrib and activating paccache.timer"
-pacman -S --needed --noconfirm pacman-contrib
+echo "Installing pacman-contrib and activating paccache.timer..."
+pacman_pkg pacman-contrib
 systemctl enable --now paccache.timer
 
-# XDG dirs
-pacman -S --needed --noconfirm xdg-user-dirs xdg-utils
+echo "Installing zsh packages..."
+zsh_pkgs=(
+    zsh
+    zsh-syntax-highlighting
+    zsh-autosuggestions
+    zsh-completions
+)
+pacman_pkg "${zsh_pkgs[@]}"
+
+echo "Installing Intel graphics packages..."
+intel_graphics_pkgs=(
+    mesa
+    mesa-utils
+    intel-media-driver
+    libva-utils
+)
+pacman_pkg "${intel_graphics_pkgs[@]}"
+
+echo "Installing Pipewire and other sound packages..."
+sound_pkgs=(
+    pipewire
+    pipewire-audio
+    pipewire-alsa
+    pipewire-pulse
+    pipewire-jack
+    wireplumber
+    pavucontrol
+)
+# NOTE: will be prompted to replace jack2 with pipewire-jack, so respond with "y" since
+# default is "N"
+echo "y" | pacman -S --needed "${sound_pkgs[@]}"
+
+echo "Using rfkill to disable bluetooth..."
+rfkill block bluetooth
+
+echo "Installing system maintenance packages..."
+sys_maint_packages=(
+    libsmbios
+    thermald
+    trash-cli
+    util-linux
+)
+pacman_pkg "${sys_maint_packages[@]}"
+echo "Enabling and starting thermald.service..."
+systemctl enable --now thermald.service
+echo "Enabling and starting fstrim.timer..."
+systemctl enable --now fstrim.timer
+
+# sets zsh as user shell
+setup_and_switch_user
+
+# XDG user dirs
+pacman_pkg xdg-user-dirs xdg-utils
 xdg-user-dirs-update 
 
-# paru
-paru_pkg() {
-    paru -S --needed --noconfirm "$1"
-}
+# TODO: not sure if can use this part since requires GUI for Firefox
+# echo "Setting up GitHub SSH key..."
+# mkdir "$HOME/.ssh"
+# ssh-keygen -t ed25519 -f "$HOME/.ssh/github" -C "github"
+# cat "$HOME/.ssh/github.pub" > wl-copy
+# read -r -p "Public SSH key copied to clipboard. Add to GitHub and press 'y' when done to continue: " confirm && [[ $confirm = [yY] ]]
+# echo "Testing SSH key..."
+# ssh -T git@github.com
 
+# TODO: stow all or just paru?
+echo "Cloning and stowing dotfiles..."
+cd "$HOME"
+# NOTE: if no SSH key added, can't use this URL for clone
+# git clone git@github.com:winter-again/.dotfiles.git
+git clone https://github.com/winter-again/.dotfiles.git
+cd "$HOME/.dotfiles"
+just stow
+git remote set-url origin git@github.com:winter-again/.dotfiles.git
+cd "$HOME"
+
+# paru
 echo "Installing paru"
 pacman -S --needed --noconfirm rustup
 rustup default stable
@@ -80,18 +197,6 @@ git clone https://aur.archlinux.org/paru.git "$PARU_REPO"
 cd "$PARU_REPO"
 makepkg -si --noconfirm
 cd "$HOME"
-
-# zsh
-echo "Installing zsh packages..."
-zsh_pkgs=(
-    zsh
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-    zsh-completions
-)
-paru_pkg "${zsh_pkgs[@]}"
-echo "Changing user shell to zsh..."
-chsh -s /usr/bin/zsh
 
 echo "Installing Sway and other desktop packages..."
 sway_pkgs=(
@@ -123,7 +228,6 @@ essential_pkgs=(
     fzf
     ghostty
     jaq
-    jq
     just
     oh-my-posh-bin
     openssh
@@ -148,7 +252,7 @@ font_pkgs=(
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
-    ttf-liberation # need for Arial in Google Docs and maybe R Studio too
+    ttf-liberation # need for Arial in Google Docs and maybe R Studio
 )
 paru_pkg "${font_pkgs[@]}"
 
@@ -159,41 +263,27 @@ network_pkgs=(
 )
 paru_pkg "${network_pkgs[@]}"
 
-# TODO: not sure if can use this part since requires GUI for Firefox
-# echo "Setting up GitHub SSH key..."
-# mkdir "$HOME/.ssh"
-# ssh-keygen -t ed25519 -f "$HOME/.ssh/github" -C "github"
-# cat "$HOME/.ssh/github.pub" > wl-copy
-# read -r -p "Public SSH key copied to clipboard. Add to GitHub and press 'y' when done to continue: " confirm && [[ $confirm = [yY] ]]
-# echo "Testing SSH key..."
-# ssh -T git@github.com
-
-echo "Cloning and stowing dotfiles..."
-cd "$HOME"
-# NOTE: if no SSH key added, can't use this URL for clone
-# git clone git@github.com:winter-again/.dotfiles.git
-git clone https://github.com/winter-again/.dotfiles.git
-cd "$HOME/.dotfiles"
-git remote set-url origin git@github.com:winter-again/.dotfiles.git
-just stow
-cd "$HOME"
-
 nvim_pkgs=(
+    base-devel
     cmake
-    unzip
+    curl
     ninja
     tree-sitter-cli
+    unzip
 )
 echo "Building and installing neovim from source..."
 paru_pkg "${nvim_pkgs[@]}"
-git clone "git@github.com:neovim/neovim.git" "$BUILD_REPOS/neovim"
+# NOTE: if no SSH key added, can't use this URL for clone
+# git clone git@github.com:neovim/neovim.git "$BUILD_REPOS/neovim"
+git clone https://github.com/neovim/neovim.git "$BUILD_REPOS/neovim"
 cd "$BUILD_REPOS/neovim"
 git checkout nightly
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
+git remote set-url origin git@github.com:neovim/neovim.git
 cd "$HOME"
 
-lua_pkgs=(
+lua_dev_pkgs=(
     lua-language-server
     selene
     stylua
@@ -228,7 +318,7 @@ dev_pkgs=(
 )
 install_dev_pkgs() {
     echo "Installing Lua dev packages..."
-    paru_pkg "${lua_pkgs[@]}"
+    paru_pkg "${lua_dev_pkgs[@]}"
 
     echo "Installing Python dev packages..."
     paru_pkg "${python_dev_pkgs[@]}"
@@ -248,61 +338,6 @@ install_dev_pkgs() {
     paru_pkg "${dev_pkgs[@]}"
 }
 
-echo "Installing Intel graphics packages..."
-intel_pkgs=(
-    mesa
-    mesa-utils
-    intel-media-driver
-    libva-utils
-)
-paru_pkg "${intel_pkgs[@]}"
-
-echo "Installing pipewire and other sound packages..."
-sound_pkgs=(
-    pipewire
-    pipewire-audio
-    pipewire-alsa
-    pipewire-pulse
-    pipewire-jack
-    wireplumber
-    pavucontrol
-)
-# NOTE: prompted to replace jack2 with pipewire-jack
-paru_pkg "${sound_pkgs[@]}"
-
-echo "Using rfkill to disable bluetooth..."
-rfkill block bluetooth
-
-sys_maint_packages=(
-    thermald
-    libsmbios
-    util-linux
-    trash-cli
-)
-echo "Installing system maintenance packages..."
-paru_pkg "${sys_maint_packages[@]}"
-echo "Enabling and starting thermald.service..."
-systemctl enable --now thermald.service
-echo "Enabling and starting fstrim.timer..."
-systemctl enable --now fstrim.timer
-
-echo "Disabling suspend on lid close..."
-sed -i "s/^#\{0,1\}HandleLidSwitch=.*/HandleLidSwitch=ignore" /etc/systemd/logind.conf
-sed -i "s/^#\{0,1\}HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=ignore" /etc/systemd/logind.conf
-sed -i "s/^#\{0,1\}HandleLidSwitchDocked=.*/HandleLidSwitchDocked=ignore" /etc/systemd/logind.conf
-
-echo "Disabling wake on lid open..."
-cat <<EOF > /etc/systemd/system/toggle-lid-wakeup.service
-[Unit]
-Description="Disable LID0 wake from suspend trigger"
-
-[Service]
-ExecStart=/bin/sh -c "/bin/echo LID0 > /proc/acpi/wakeup"
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
 echo "Installing theming packages..."
 theming_pkgs=(
     nwg-look
@@ -321,14 +356,14 @@ xdg_portal_pkgs=(
 )
 paru_pkg "${xdg_portal_pkgs[@]}"
 
-echo "Installing Nemo..."
+echo "Installing Nemo and setting as default file manager..."
 paru_pkg nemo
 # set as default file browser
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 # set Wezterm as terminal for Nemo
 gsettings set org.cinnamon.desktop.default-applications.terminal exec wezterm
 
-echo "Installing graphical apps..."
+echo "Installing other apps..."
 app_pkgs=(
     zathura
     zathura-pdf-mupdf
@@ -340,7 +375,17 @@ app_pkgs=(
 )
 paru_pkg "${app_pkgs[@]}"
 
-# TODO: install rclone and restic and setup backup services
+echo "Installing rclone and restic for backups..."
+paru_pkg rclone restic
+# NOTE: if no SSH key added, can't use this URL for clone
+# git clone git@github.com:winter-again/backups.git
+git clone https://github.com/winter-again/backups.git "$HOME/Documents/code/backups"
+cd "$HOME/Documents/code/backups"
+just lab
+just personal
+git remote set-url origin git@github.com:winter-again/backups.git
+cd "$HOME"
+
 # TODO: CUPS for printing?
 
 echo "Installing ufw and activating firewall..."
@@ -348,9 +393,8 @@ paru_pkg ufw
 systemctl enable --now ufw.service
 ufw enable
 
-echo "Done. Rebooting in 3 seconds..."
-sleep 3
-reboot
+clear
+echo "Done. Reboot the system."
 
 ###########
 
@@ -422,44 +466,6 @@ format_mount_partitions() {
     lsblk
 }
 
-setup_user() {
-    clear
-    read -r -p "Enter username: " USER
-
-    read -r -p "Enter user password: " PASSWD
-    read -r -p "Confirm password: " PASSWD2
-    while ! [ "$PASSWD" = "$PASSWD2" ]; do
-        read -r -p "Enter user password: " PASSWD
-        read -r -p "Confirm password: " PASSWD2
-    done
-
-    echo "$USER"
-
-    echo "Creating user..."
-    useradd -m -G wheel -s /bin/bash "$USER"
-
-    echo "Setting user password..."
-    # should prompt user
-    # passwd "$USER"
-    echo "$USER:$PASSWD" | chpasswd
-    unset PASSWD
-    unset PASSWD2
-
-    echo "Making wheel group sudoers..."
-    arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
-}
-
-# install_base() {
-#     echo "Installing base system packages..."
-#     pacstrap -K /mnt "${base_pacstrap_pkgs[@]}"
-# }
-#
-# gen_fstab() {
-#     echo "Generating fstab..."
-#     genfstab -U /mnt >> /mnt/etc/fstab
-#     sleep 2
-# }
-#
 # set_timezone() {
 #     echo "Setting timezone to $TIMEZONE..."
 #     arch-chroot /mnt ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
@@ -476,21 +482,4 @@ setup_user() {
 #     echo "LANG=$LOCALE" > /mnt/etc/locale.conf
 #     echo "Check the locale.conf below..."
 #     cat /mnt/etc/locale.conf
-# }
-#
-# set_host() {
-#     echo "$HOSTNAME" > /mnt/etc/hostname
-#     cat << EOF > /mnt/etc/hosts
-#         127.0.0.1    localhost
-#         ::1          localhost
-#         127.0.1.1    $HOSTNAME
-# EOF
-# }
-#
-# install_system() {
-#     echo "Installing desktop packages..."
-#     arch-chroot /mnt pacman -S "${desktop_pkgs[@]}"
-#
-#     echo "Installing essential packages..."
-#     arch-chroot /mnt pacman -S "${essential_pkgs[@]}"
 # }
