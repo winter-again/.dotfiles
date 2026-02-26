7aee2ab676ebf977af958fd7279c51aa977e0c40
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:09:54 -0500

    comment keymap

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,3 +1,4 @@
+--- @diagnostic disable: missing-fields
 return {
     'numToStr/Comment.nvim',
     event = { 'BufReadPost', 'BufNewFile' },
@@ -7,20 +8,20 @@ return {
             -- toggles commenting for current line
             toggler = {
                 -- line = '<C-_>',
-                line = 'gcc',
-                block = 'gcb',
+                line = 'mcc',
+                block = 'mcb',
             },
             -- LHS of operator-pending maps in normal and visual mode
             -- can use to comment two lines down with 'gb2j' for ex or after selection
             opleader = {
                 -- line = '<C-_>',
-                line = 'gc',
-                block = 'gb',
+                line = 'mc',
+                block = 'mb',
             },
             extra = {
-                above = 'gcO',
-                below = 'gco',
-                eol = 'gce',
+                above = 'mcO',
+                below = 'mco',
+                eol = 'mce',
             },
             pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
         })
