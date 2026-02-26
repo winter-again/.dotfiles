5c712b458b1ff3701f35da1233f3a2a5c87dd5fd
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:09:53 -0500

    fix lualine lsp, formatters, venv indicators

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,68 +4,53 @@
 -- but lualine component won't know and thus it won't be shown
 -- inspired from here: https://github.com/nvim-lualine/lualine.nvim/issues/277
 local function get_venv()
+    local output = ''
     local venv = vim.env.VIRTUAL_ENV
     if venv then
-        local venv_text = string.match(venv, '([^/]+)$')
-        return venv_text
-    else
-        return ''
+        output = string.match(venv, '([^/]+)$')
+        output = string.format('(%s)', output)
     end
+    return output
 end
 
 -- display attached LSP client if there is one
--- from this ex: https://github.com/nvim-lualine/lualine.nvim/blob/master/examples/evil_lualine.lua
 local function get_lsp()
-    local msg = 'None active'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
     local clients = vim.lsp.get_active_clients()
     if next(clients) == nil then
-        return msg
-    end
-    for _, client in ipairs(clients) do
-        local filetypes = client.config.filetypes
-        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= 'null-ls' then
-            return client.name
-        end
-    end
-    return msg
-end
-
-local function get_null_ls()
-    local buf_ft = vim.bo.filetype
-    local sources = require('null-ls').get_source({ filetype = buf_ft })
-    if next(sources) ~= nil then
-        local src_list = {}
-        for _, source in pairs(sources) do
-            table.insert(src_list, source.name)
-        end
-        local str_sources = table.concat(src_list, ', ')
-        local msg = string.format('[%s]', str_sources)
-        return msg
-    else
         return 'None active'
+    else
+        for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+            end
+        end
     end
 end
 
--- combine these two functions into a single section's output
--- a bit hacky but checks if there's an active virtual environment and
--- adds it to the LSP indicator
+-- display formatter information from conform if avail
+local function get_conform()
+    local output = ''
+    local formatters = require('conform').list_formatters(0)
+    if next(formatters) == nil then
+        return output
+    else
+        local names = {}
+        for _, formatter in ipairs(formatters) do
+            table.insert(names, formatter.name)
+        end
+        output = table.concat(names, ', ')
+        output = string.format('[%s]', output)
+        return output
+    end
+end
+
 local function display_lsp_venv()
     local venv_name = get_venv()
     local lsp_status = get_lsp()
-    local null_ls = get_null_ls()
-    if lsp_status ~= 'None active' and null_ls ~= 'None active' and venv_name ~= '' then
-        return lsp_status .. null_ls .. ' (' .. venv_name .. ')'
-    elseif lsp_status ~= 'None active' and null_ls ~= 'None active' and venv_name == '' then
-        return lsp_status .. null_ls
-    elseif lsp_status ~= 'None active' and null_ls == 'None active' and venv_name ~= '' then
-        return lsp_status .. ' (' .. venv_name .. ')'
-        -- elseif lsp_status ~= 'None active' and null_ls == 'None active' and venv_name == '' then
-        -- return lsp_status
-        -- elseif lsp_status == 'None active' then
-    else
-        return lsp_status
-    end
+    local formatter = get_conform()
+    return lsp_status .. formatter .. venv_name
 end
 
 -- use gitsigns as the diff source below
