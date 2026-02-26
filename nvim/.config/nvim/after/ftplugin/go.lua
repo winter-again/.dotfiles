ab2581e1b2f4f6afc4d320dd270ab14db31c243e
winter-again <63322884+winter-again@users.noreply.github.com>
2026-02-26 07:10:02 -0500

    Updates

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -1,4 +1,1 @@
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4 -- look/feel of tab and backspace when editing
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false -- don't expand tabs into spaces according to Go style rules
+vim.opt.expandtab = false -- gofmt uses tabs instead of spaces
