a3587419bba29dd0249e1d82df6eada4c3a4f36a
winter-again <63322884+winter-again@users.noreply.github.com>
2026-04-25 21:58:17 -0400

    Updates

---
+++ nvim/.config/nvim/after/lsp/ty.lua
@@ -0,0 +1,16 @@
+return {
+    settings = {
+        ty = {
+            completions = {
+                autoImport = false,
+            },
+            -- diagnosticMode = "workspace",
+            -- NOTE: I think I toggle these anyway so only need to set false if absolutely
+            -- don't want capability
+            -- inlayHints = {
+            --     variableTypes = false,
+            --     callArgumentNames = false,
+            -- },
+        },
+    },
+}
--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -3,15 +3,15 @@ return {
     event = "InsertEnter",
     config = function()
         local npairs = require("nvim-autopairs")
-        local rule = require("nvim-autopairs.rule")
+        -- local rule = require("nvim-autopairs.rule")
         npairs.setup()
-        npairs.add_rules({
-            rule("$", "$", {
-                "tex",
-                "latex",
-                "markdown", -- need both markdown lines?
-                "markdown_inline",
-            }),
-        })
+        -- npairs.add_rules({
+        --     rule("$", "$", {
+        --         "tex",
+        --         "latex",
+        --         "markdown", -- need both markdown lines?
+        --         "markdown_inline",
+        --     }),
+        -- })
     end,
 }
